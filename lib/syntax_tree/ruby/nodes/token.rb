8d63111095126cc8ee454ab30db19c83732a7d3c
Rolf Timmermans <r.timmermans@voormedia.com>
2011-04-06 01:18:33 +0200

    Visitor is even simpler.

--- lib/syntax_tree/ruby/nodes/token.rb
+++ lib/syntax_tree/ruby/nodes/token.rb
@@ -5,7 +5,9 @@ require "syntax_tree/ruby/nodes/aspects/epilogued"
 module SyntaxTree
   module Ruby
     class Token < Node
-      define_nodes :token, :position
+      define_nodes :token
+
+      attr_accessor :position
 
       include Prologued
     end
--- lib/syntax_tree/visitors/to_ruby.rb
+++ lib/syntax_tree/visitors/to_ruby.rb
@@ -3,12 +3,12 @@ require "syntax_tree/visitors/base"
 module SyntaxTree
   module Visitors
     class ToRuby < Base
-      def visit_ruby_node(o)
-        o.nodes.collect { |node| visit node }.join
+      def visit_ruby_node(base)
+        base.nodes.each_with_object("") { |node, string| string << visit(node) }
       end
 
-      def visit_ruby_token(o)
-        o.prologue.to_s + o.token
+      def visit_string(string)
+        string
       end
     end
   end
